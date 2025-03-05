c204efb32908d61d18837509d9a364a1a53a1dd2
priya darshini p <you@example.com>
2025-03-05 19:21:48 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import jsPDF from 'jspdf';
-import logo from './image/LOGO.png'; 
-import stamp from './image/paid.png'; 
-import paid from './image/seal.png';
-import 'jspdf-autotable';
+import logo from './image/LOGO.jpg'; 
+import stamp from './image/paid.jpg'; 
+import paid from './image/paid.png';
+import 'jspdf-autotable'; 
 import './App.css';
 
 const ResidentForm = () => {
@@ -17,7 +17,6 @@ const ResidentForm = () => {
     sharing: 'Single',
     rentMonth: 'Ongoing',
     receiptType: 'Rent', 
-    tenure:'',
   });
 
   const handleChange = (e) => {
@@ -27,24 +26,20 @@ const ResidentForm = () => {
 
   const generatePDF = () => {
     const doc = new jsPDF('p', 'pt', 'a4');
-    doc.addImage(logo, 'PNG', 200, 35, 200, 150);
+    doc.addImage(logo, 'PNG', 200, -10, 200, 200);
     doc.setFontSize(10);
     doc.setTextColor(0, 0, 0);
     doc.text('#26  PEERLESS COLONY ,NEAR GARDEN CITY COLLEGE ,BHATTRAHALLI , KR PURAM 560049', 70, 180);
     doc.setFontSize(24);
-    doc.setTextColor(0, 0, 0);
-    doc.setFont('helvetica', 'bold');
-    doc.text('#SAHANA LADIES PG', 170, 205);
-    
-    doc.setFontSize(30);
     doc.setFont('helvetica', 'bold');
     doc.setTextColor(255, 0, 0);
   
+    // Center the title dynamically
     const title = formData.receiptType === 'Rent' ? 'RENT PAYMENT RECEIPT' : 'DEPOSIT PAYMENT RECEIPT';
     const pageWidth = doc.internal.pageSize.getWidth();
     const textWidth = doc.getTextWidth(title);
     const xCoordinate = (pageWidth - textWidth) / 2;
-    doc.text(title, xCoordinate, 240);
+    doc.text(title, xCoordinate, 210);
   
     const tableData = formData.receiptType === 'Rent'
       ? [
@@ -63,7 +58,6 @@ const ResidentForm = () => {
           ['Mode of Payment:', formData.modeOfPayment],
           ['Deposit Amount:', formData.amount],
           ['Date of Payment:', formData.dateOfPayment],
-          ['Agreement Tenure: ',formData.tenure]
         ];
   
     doc.autoTable({
@@ -85,10 +79,10 @@ const ResidentForm = () => {
       },
     });
   
-    doc.addImage(stamp, 'PNG', 300, doc.autoTable.previous.finalY + 50, 120, 70);
+    doc.addImage(stamp, 'PNG', 300, doc.autoTable.previous.finalY + 50, 280, 70);
     doc.setFontSize(16);
     doc.setTextColor(0, 0, 0);
-    doc.addImage(paid, 'JPG', 130, doc.autoTable.previous.finalY + 10, 180, 150); 
+    doc.addImage(paid, 'JPG', 90, 600, 150, 150); 
   
     const fileName = `${formData.name}_${formData.dateOfPayment}_${formData.receiptType}Receipt.pdf`;
     doc.save(fileName);
@@ -137,13 +131,6 @@ const ResidentForm = () => {
             Date of Payment:
             <input type="date" name="dateOfPayment" value={formData.dateOfPayment} onChange={handleChange} required className="input" />
           </label>
-          {formData.receiptType === 'Deposit' &&(
-            <>
-            <label className="label">
-            Agreement Tenure:
-            <input type="text" name="tenure" value={formData.tenure} onChange={handleChange} required className="input" />
-          </label></>
-          )}
           {formData.receiptType === 'Rent' && (
             <>
               <label className="label">
@@ -182,152 +169,4 @@ const ResidentForm = () => {
   );
 };
 
-// export default ResidentForm;
-// import React, { useState } from 'react';
-// import jsPDF from 'jspdf';
-// import logo from './image/LOGO.png'; 
-// import stamp from './image/paid.png'; 
-// import paid from './image/seal.png';
-// import 'jspdf-autotable';
-// import './App.css';
-
-// const ResidentForm = () => {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     roomNumber: '',
-//     modeOfPayment: 'Cash',
-//     amount: '',
-//     dateOfPayment: '',
-//     overstandingDues: '',
-//     sharing: 'Single',
-//     rentMonth: 'Ongoing',
-//     receiptType: 'Rent',
-//     tenure: '',
-//     vacatingDate: '',
-//     depositCleared: 'Yes',
-//     remainingDeposit: '',
-//     approvalDate: '',
-//     damage: 'No',
-//     damageDetails: '',
-//     parentApproval: '',
-//   });
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({ ...formData, [name]: value });
-//   };
-
-//   const generatePDF = () => {
-//     const doc = new jsPDF('p', 'pt', 'a4');
-//     doc.addImage(logo, 'PNG', 200, 35, 200, 150);
-//     doc.setFontSize(10);
-//     doc.text('#26 PEERLESS COLONY, NEAR GARDEN CITY COLLEGE, BHATTRAHALLI, KR PURAM 560049', 70, 180);
-//     doc.setFontSize(24);
-//     doc.setFont('helvetica', 'bold');
-//     doc.text('#SAHANA LADIES PG', 170, 205);
-    
-//     doc.setFontSize(30);
-//     doc.setTextColor(255, 0, 0);
-//     const title =
-//       formData.receiptType === 'Rent'
-//         ? 'RENT PAYMENT RECEIPT'
-//         : formData.receiptType === 'Deposit'
-//         ? 'DEPOSIT PAYMENT RECEIPT'
-//         : 'VACATING RECEIPT';
-//     const pageWidth = doc.internal.pageSize.getWidth();
-//     const textWidth = doc.getTextWidth(title);
-//     doc.text(title, (pageWidth - textWidth) / 2, 240);
-
-//     let tableData = [];
-//     if (formData.receiptType === 'Rent') {
-//       tableData = [
-//         ['Resident Name:', formData.name],
-//         ['Room No:', formData.roomNumber],
-//         ['Mode of Payment:', formData.modeOfPayment],
-//         ['Rent Amount:', formData.amount],
-//         ['Date of Payment:', formData.dateOfPayment],
-//         ['Overstanding Dues:', formData.overstandingDues],
-//         ['Sharing:', formData.sharing],
-//         ['Rent Month:', formData.rentMonth],
-//       ];
-//     } else if (formData.receiptType === 'Deposit') {
-//       tableData = [
-//         ['Resident Name:', formData.name],
-//         ['Room No:', formData.roomNumber],
-//         ['Mode of Payment:', formData.modeOfPayment],
-//         ['Deposit Amount:', formData.amount],
-//         ['Date of Payment:', formData.dateOfPayment],
-//         ['Agreement Tenure:', formData.tenure],
-//       ];
-//     } else {
-//       tableData = [
-//         ['Resident Name:', formData.name],
-//         ['Room No:', formData.roomNumber],
-//         ['Vacating Date:', formData.vacatingDate],
-//         ['Deposit Cleared:', formData.depositCleared],
-//         formData.depositCleared === 'No' ? ['Remaining Deposit:', formData.remainingDeposit] : null,
-//         ['Approval Date:', formData.approvalDate],
-//         ['Damage:', formData.damage],
-//         formData.damage === 'Yes' ? ['Damage Details:', formData.damageDetails] : null,
-//         ['Parent Approval:', formData.parentApproval],
-//       ].filter(Boolean);
-//     }
-
-//     doc.autoTable({
-//       body: tableData,
-//       startY: 250,
-//       styles: { fontSize: 24, textColor: 0 },
-//       theme: 'plain',
-//       columnStyles: { 0: { fontStyle: 'bold', cellWidth: 250 }, 1: { cellWidth: 'auto' } },
-//     });
-
-//     doc.addImage(stamp, 'PNG', 300, doc.autoTable.previous.finalY + 50, 120, 70);
-//     doc.addImage(paid, 'JPG', 130, doc.autoTable.previous.finalY + 10, 180, 150);
-
-//     const fileName = `${formData.name}_${formData.dateOfPayment}_${formData.receiptType}Receipt.pdf`;
-//     doc.save(fileName);
-//   };
-
-//   return (
-//     <div className="container">
-//       <div className="form">
-//         <h2 className="heading">Generate PDF Receipt</h2>
-//         <form onSubmit={(e) => { e.preventDefault(); generatePDF(); }}>
-//           <label className="label">Receipt Type:
-//             <select name="receiptType" value={formData.receiptType} onChange={handleChange} className="input">
-//               <option value="Rent">Rent</option>
-//               <option value="Deposit">Deposit</option>
-//               <option value="Vacating">Vacating</option>
-//             </select>
-//           </label>
-          
-//           <label className="label">Name: <input type="text" name="name" value={formData.name} onChange={handleChange} className="input" required /></label>
-//           <label className="label">Room Number: <input type="number" name="roomNumber" value={formData.roomNumber} onChange={handleChange} className="input" required /></label>
-
-//           {formData.receiptType === 'Vacating' && (
-//             <>
-//               <label className="label">Vacating Date: <input type="date" name="vacatingDate" value={formData.vacatingDate} onChange={handleChange} className="input" required /></label>
-//               <label className="label">Deposit Cleared:
-//                 <select name="depositCleared" value={formData.depositCleared} onChange={handleChange} className="input">
-//                   <option value="Yes">Yes</option>
-//                   <option value="No">No</option>
-//                 </select>
-//               </label>
-//               {formData.depositCleared === 'No' && <label className="label">Remaining Deposit: <input type="number" name="remainingDeposit" value={formData.remainingDeposit} onChange={handleChange} className="input" /></label>}
-//               <label className="label">Damage:
-//                 <select name="damage" value={formData.damage} onChange={handleChange} className="input">
-//                   <option value="No">No</option>
-//                   <option value="Yes">Yes</option>
-//                 </select>
-//               </label>
-//               {formData.damage === 'Yes' && <label className="label">Damage Details: <input type="text" name="damageDetails" value={formData.damageDetails} onChange={handleChange} className="input" /></label>}
-//             </>
-//           )}
-//           <button type="submit" className="button">Generate PDF</button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ResidentForm;
+export default ResidentForm;
